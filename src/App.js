52e692a4044e1e505489fd4e5eb41bd49da47369
arbaaz <abdularbaaz@gmail.com>
2018-05-26 23:33:09 +0530

    --wip-- [skip ci]

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { Component } from "react";
+import request from "superagent";
 import "./style.css";
 
-const Student = ({ name }) => <li className="student">{name}</li>;
+const drawCards = id =>
+  request.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
 
 export default class App extends Component {
   state = {
-    data: [
-      {
-        name: "Aman"
-      },
-      {
-        name: "Aastha"
-      },
-      {
-        name: "Rohit"
-      },
-      {
-        name: "Harshit"
-      }
-    ]
+    deckId: "",
+    playerOneCards: []
   };
 
+  drawPlayerOneCards = () => {
+    drawCards(this.state.deckId).then(result => {
+      this.setState({
+        playerOneCards: result.body.cards
+      });
+    });
+  };
+
+  componentDidMount() {
+    request
+      .get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+      .then(response => {
+        this.setState({
+          deckId: response.body.deck_id
+        });
+      });
+  }
+
   render() {
-    const { data } = this.state;
     return (
-      <ul className="studentList">
-        {data.map((item, index) => <Student name={item.name} />)}
-      </ul>
+      <div>
+        <button onClick={this.drawPlayerOneCards}>Draw Cards</button>
+        {this.state.deckId}
+        {this.state.playerOneCards.map((card, index) => (
+          <img src={card.images.png} key={index} alt="img" />
+        ))}
+      </div>
     );
   }
 }
